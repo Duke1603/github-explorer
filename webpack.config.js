6558a730df508875b7b6fbddbd309695c9934fde
Vinícius Mourão <vinicius.mourao23@gmail.com>
2021-03-18 16:52:15 -0300

    configura repository

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,39 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-const isDevelopment = process.env.NODE_ENV !== 'production'; 
-
+ 
+const isDevelopment = process.env.NODE_ENV !== 'production'
+ 
 module.exports = {
-    mode: isDevelopment ? 'development' : 'production',
-    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'), 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'] 
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin ({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
-
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    }
-
-};
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname,'src','index.jsx'),
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename:'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js','.jsx']
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname,'public'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname,'public','index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/, //will convert if the file is jsx
+        exclude: /node_modules/, //will ignore all the files from node modules
+        use: 'babel-loader' //will use babel-loader to transpile every jsx file
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader','css-loader', 'sass-loader']
+      }
+    ]
+  }
+}
