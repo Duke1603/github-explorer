5253843e4a03e7f40cdf88bc30a592c1b8bfca8d
Vinícius Mourão <vinicius.mourao23@gmail.com>
2021-03-25 18:27:40 -0300

    ensina função 'map' e  'key'

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,12 +4,6 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
 
-const repository = {
-    name: 'unform', 
-    description: 'Forms in React',
-    link: 'https://github.com/uinform/unform'
-}
-
 export function RepositoryList(){
     const [repositories, setRepositories] = useState([]); 
 
@@ -23,10 +17,9 @@ export function RepositoryList(){
         <section className="repository-list"> 
             <h1>Lista de repositórios</h1>
                 <ul>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repository={repository}/>
-                    <RepositoryItem repositoryµ={repository}/>
+                    {repositories.map(repository => {
+                        return <RepositoryItem Key={repository.name} repository={repository}/>
+                    })}
                 </ul>
         </section>
     );
